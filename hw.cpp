2f89641b47a34f2d50ab14543dde23750ba072c2
wrehe0 <olgaklukina856@mail.ru>
2026-01-26 12:25:21 +0000

    update

--- hw.cpp
+++ hw.cpp
@@ -1,12 +1,27 @@
 #include <iostream> 
 using namespace std;
-int main(){
+/*int main(){
     setlocale(LC_ALL, "RU");
 
-    cout << "Hello World" << "\n";
+   /* cout << "Hello World" << "\n";
     cerr << "ERROR" << "\n";
     int a;
     cin >> a;
-    return a;
+    return a;*/
+
+    /*int socks[12];
+    for (int i = 0; i < 12; i++){
+        socks[i] = i;
+    }
+    for(int i = 0; i < 12; i++){
+        cout << "Socks [" << i << "] = " << socks[i] << "\n";
+    }*/
+    
+    int main (int argc, char* argv[]){
+        cout << "Длинна массива : " << argc << endl;
+    for (int i = 0; i < argc; ++i) {
+        cout << "Аргумент " << i << ": " << argv[i] << endl;
+    }
 }
-/**/
+/*соло на клавиатуре найти потом*/
+/*продолжить на ldd ./hw*/
