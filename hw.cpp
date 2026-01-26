64d141458bd8dfe9e49366a2237d7db6374a7fea
wrehe0 <olgaklukina856@mail.ru>
2026-01-26 10:35:04 +0000

    abc

---
+++ hw.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+using namespace std;
+int main(){
+    setlocale(LC_ALL, "RU");
+
+    cout << "Hello World" << "\n";
+    cerr << "ERROR" << "\n";
+    int a;
+    cin >> a;
+    return a;
+}
+/**/
