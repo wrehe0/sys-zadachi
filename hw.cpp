6eeda2bf059644482b0b058218db3babbbe900a3
wrehe0 <olgaklukina856@mail.ru>
2026-01-28 20:03:08 +0000

    отчет сделала

--- hw.cpp
+++ hw.cpp
@@ -1,27 +1,32 @@
 #include <iostream> 
 using namespace std;
-/*int main(){
-    setlocale(LC_ALL, "RU");
-
-   /* cout << "Hello World" << "\n";
-    cerr << "ERROR" << "\n";
-    int a;
-    cin >> a;
-    return a;*/
-
-    /*int socks[12];
-    for (int i = 0; i < 12; i++){
-        socks[i] = i;
-    }
-    for(int i = 0; i < 12; i++){
-        cout << "Socks [" << i << "] = " << socks[i] << "\n";
-    }*/
-    
-    int main (int argc, char* argv[]){
+int main (int argc, char* argv[]){
+        setlocale(LC_ALL, "RU");
         cout << "Длинна массива : " << argc << endl;
     for (int i = 0; i < argc; ++i) {
         cout << "Аргумент " << i << ": " << argv[i] << endl;
     }
 }
+
+
+
+
+
+
 /*соло на клавиатуре найти потом*/
 /*продолжить на ldd ./hw*/
+
+int main(){
+    cout << "Hello World" << "\n";
+    cerr << "ERROR" << "\n";
+    int a;
+    cin >> a;
+    return a;
+int socks[12];
+    for (int i = 0; i < 12; i++){
+        socks[i] = i;
+    }
+    for(int i = 0; i < 12; i++){
+        cout << "Socks [" << i << "] = " << socks[i] << "\n";
+    }
+}
